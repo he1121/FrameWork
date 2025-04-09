4c1d5123c84de93e63e0e3807e520d1dd081e0f8
he1121 <1253655691@qq.com>
2025-04-09 21:27:20 +0800

    首次提交

---
+++ Assets/ZMUIFrameWork/Script/Runtime/Base/WindowBase.cs
@@ -0,0 +1,126 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.UI;
+
+public class WindowBase : WindowBehaviour
+{
+    private List<Button> mAllButtonList = new List<Button>(); //所有button列表
+    private List<Toggle> mAllToggleList = new List<Toggle>(); //所有toggle列表
+    private List<InputField> mAllInputFieldList = new List<InputField>(); //所有输入框列表
+    
+    #region 生命周期函数
+    public override void OnAwake()
+    {
+        base.OnAwake();
+    }
+
+    public override void OnShow()
+    {
+        base.OnShow();
+    }
+
+    public override void OnUpdate()
+    {
+        base.OnUpdate();
+    }
+
+    public override void OnHide()
+    {
+        base.OnHide();
+    }
+
+    public override void OnDestroy()
+    {
+        base.OnDestroy();
+        RemoveAllButtonClickListener();
+        removeAllToggleClickListener();
+        RemoveAllInputFieldClickListener();
+        
+        mAllButtonList.Clear();
+        mAllToggleList.Clear();
+        mAllInputFieldList.Clear();
+    }
+
+    public override void SetVisible(bool isVisible)
+    {
+        base.SetVisible(isVisible);
+    }
+
+    #endregion
+
+    #region 事件管理
+
+    public void AddButtonClickListener(Button btn, UnityAction action)
+    {
+        if (btn != null)
+        {
+            if (!mAllButtonList.Contains(btn))
+            {
+                mAllButtonList.Add(btn);
+                btn.onClick.RemoveAllListeners();
+                btn.onClick.AddListener(action);
+            }
+        }
+    }
+
+    public void AddToggleClickListener(Toggle toggle, UnityAction<bool, Toggle> action)
+    {
+        if (toggle != null)
+        {
+            if (!mAllToggleList.Contains(toggle))
+            {
+                mAllToggleList.Add(toggle);
+            }
+            toggle.onValueChanged.RemoveAllListeners();
+            toggle.onValueChanged.AddListener((isOn) =>
+            {
+                action?.Invoke(isOn, toggle);
+            });
+        }
+    }
+
+    public void AddInputFieldClickListener(InputField inputField, UnityAction<string> onChangeAction,
+        UnityAction<string> onEndAction)
+    {
+        if (inputField != null)
+        {
+            if (!mAllInputFieldList.Contains(inputField))
+            {
+                mAllInputFieldList.Add(inputField);
+            }
+            inputField.onValueChanged.RemoveAllListeners();
+            inputField.onEndEdit.RemoveAllListeners();
+            
+            inputField.onValueChanged.AddListener(onChangeAction);
+            inputField.onEndEdit.AddListener(onEndAction);
+        }
+    }
+
+    public void RemoveAllButtonClickListener()
+    {
+        foreach (var btn in mAllButtonList)
+        {
+            btn.onClick.RemoveAllListeners();
+        }
+    }
+    
+    public void removeAllToggleClickListener()
+    {
+        foreach (var toggle in mAllToggleList)
+        {
+            toggle.onValueChanged.RemoveAllListeners();
+        }
+    }
+
+    public void RemoveAllInputFieldClickListener()
+    {
+        foreach (var input in mAllInputFieldList)
+        {
+            input.onValueChanged.RemoveAllListeners();
+            input.onEndEdit.RemoveAllListeners();
+        }
+    }
+    #endregion
+}
---
+++ Assets/ZMUIFrameWork/Script/Runtime/Base/WindowBehaviour.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class WindowBehaviour
+{
+    /// <summary>
+    /// 当前窗口物体
+    /// </summary>
+    public GameObject gameobject { get; set; }
+    
+    public Transform transform { get; set; }
+    
+    public Canvas Canvas { get; set; }
+    
+    public string Name{ get; set; }
+    
+    public bool Visible { get; set; }
+    
+    public virtual void OnAwake(){}//只在物体创建时执行一次
+    
+    public virtual void OnShow(){} //物体显示时调用一次 与mono OnEnable一致
+    
+    public virtual void OnUpdate(){}
+    
+    public virtual void OnHide(){}//物体显示时调用一次 与mono OnDisable一致
+    
+    public virtual void OnDestroy(){}
+    
+    public virtual void SetVisible(bool isVisible){}
+}
